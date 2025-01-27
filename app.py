2f8a79757db1783e1cbe6a1a59570d9de93858e3
FaithKamaraju <faithkamaraju@gmail.com>
2025-01-27 11:53:50 -0500

    part 2 commit

--- app.py
+++ app.py
@@ -1,15 +1,36 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
+
+
+posts = [
+    {
+        'author': 'Faith Kamaraju',
+        'title' : 'Blog Post 1',
+        'content': 'First Post Content',
+        'date_posted': 'January 26, 2025'
+    },
+    {
+        'author': 'Maeve Guenette',
+        'title' : 'Dnd stuff',
+        'content': 'DND DND DND DND',
+        'date_posted': 'January 25, 2025'
+    }
+]
+
+
+#routes
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return "<h1> Hello World! </h1>"
+    return render_template('home.html', posts=posts)
 
 @app.route('/about')
 def about():
-    return "<h1> About Page</h1>"
+    return render_template('about.html',title='About Page')
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
